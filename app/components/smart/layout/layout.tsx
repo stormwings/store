ebeedeadfdeb26846e89d165835aa4f4d6bea26e
Mariano Villarreal <marianov994@gmail.com>
2025-03-05 00:12:04 -0300

    import navbarr

--- app/components/smart/layout/layout.tsx
+++ app/components/smart/layout/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router";
-import Navbar from "./../navbar/Navbar";
+import Navbar from "../Navbar/Navbar";
 
 export const LayoutDefault: React.FC = () => {
   return (
